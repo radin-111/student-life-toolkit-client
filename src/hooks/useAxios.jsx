ca4766e9b7f7f5073e17c1a120133324e489ff9a
radin-111 <radinsheikh87@gmail.com>
2025-09-05 16:18:06 +0600

    adding interceptor

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,13 +1,48 @@
 import axios from "axios";
 import React from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000",
-  
 });
 
 const useAxios = () => {
-    return instance;
+  const { user, handleLogout } = useAuth();
+
+  const navigate = useNavigate();
+
+  instance.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (error) => {
+      const status = error.status;
+      if (status === 403) {
+        return <Forbidden></Forbidden>;
+      } else if (status === 401) {
+        handleLogout()
+          .then(() => {
+            navigate("/login");
+          })
+          .catch(() => {});
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
 };
 
 export default useAxios;
