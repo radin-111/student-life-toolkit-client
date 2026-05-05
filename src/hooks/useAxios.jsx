0188c2e8575c938aeeb66f8c288bec7f03dfe7ab
radin-111 <radinsheikh87@gmail.com>
2026-05-05 18:45:06 +0600

    fixing some code

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,45 +1,56 @@
 import axios from "axios";
+import { useEffect } from "react";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://student-life-toolkit-server.vercel.app",
 });
 
 const useAxios = () => {
   const { user, handleLogout } = useAuth();
-
   const navigate = useNavigate();
 
-  instance.interceptors.request.use(
-    (config) => {
-      config.headers.Authorization = `Bearer ${user.accessToken}`;
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+  useEffect(() => {
+    
+    const reqInterceptor = instance.interceptors.request.use(
+      (config) => {
+        if (user?.accessToken) {
+          config.headers.Authorization = `Bearer ${user.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
-  instance.interceptors.response.use(
-    (res) => {
-      return res;
-    },
-    (error) => {
-      const status = error.status;
-      if (status === 403) {
-        return <Forbidden></Forbidden>;
-      } else if (status === 401) {
-        handleLogout()
-          .then(() => {
+    const resInterceptor = instance.interceptors.response.use(
+      (res) => res,
+      async (error) => {
+        const status = error.response?.status;
+
+        if (status === 401) {
+          try {
+            await handleLogout();
             navigate("/login");
-          })
-          .catch(() => {});
-      }
+          } catch (err) {
+            console.error(err);
+          }
+        }
 
-      return Promise.reject(error);
-    }
-  );
+        if (status === 403) {
+          navigate("/forbidden"); // redirect instead of returning JSX
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    // Cleanup interceptors (VERY IMPORTANT)
+    return () => {
+      instance.interceptors.request.eject(reqInterceptor);
+      instance.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, handleLogout, navigate]);
 
   return instance;
 };
